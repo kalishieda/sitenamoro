917c4e9769763ae5971bef9d5f1f4b270a87b01f
Marcus Vinícius <126020752+MarcusVFenix@users.noreply.github.com>
2024-02-07 22:10:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,13 +15,19 @@ function aceitou() {
 document.getElementById("namorobuttonyes").onclick = function() {
     aceitou();
 };
+/*O código foi modificado a partir desse ponto com reduções significativas de linhas e lógica complicada para algo simples, funcional e curto*/
+/*função para gerar um número aleatório diferente de 0 a partir de dois números quaisquer definidos*/
+function rand0 (min, max) {
+    var rand = Math.random() * (max - min) + min;
+    while (rand == 0){
+        rand = Math.random() * (max - min) + min;
+    }
+    return(rand)
+}
 
 function moveButton() {
     var button = document.getElementById('namorobuttonno');
-    var maxX = window.innerWidth - button.offsetWidth;
-    var maxY = window.innerHeight - button.offsetHeight;
-    var newX = Math.floor(Math.random() * maxX);
-    var newY = Math.floor(Math.random() * maxY);
-    button.style.left = newX + 'px';
-    button.style.top = newY + 'px';
-}
+    
+    /*Os valores randômicos máximos não variam, evitando problema em que o botão sai da tela*/
+    button.style.left = rand0(-1, 1) * Math.floor(Math.random() * 500) + "px";
+    button.style.top = rand0(-1, 1) * Math.floor(Math.random() * 300) + "px";
